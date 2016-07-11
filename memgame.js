a6eafec8f2bca9250b285272e4d852dbbfdf7fea
Bill Leonhardt <iamleonhardt@gmail.com>
2016-07-11 15:38:27 -0700

    v1.0 fixed card disables

--- memgame.js
+++ memgame.js
@@ -2,6 +2,7 @@ var first_card_clicked = null;
 var second_card_clicked = null;
 var total_possible_matches = 0;
 var match_counter = 0;
+var can_be_clicked = true;
 
 $(document).ready(initialize_game);
 
@@ -12,6 +13,9 @@ function initialize_game() {
 }
 
 function card_clicked() {
+    if (can_be_clicked == false){
+        return;
+    }
     var the_card = $(this);
     the_card.find(".back").hide();
     the_card.off("click");
@@ -21,9 +25,9 @@ function card_clicked() {
     }
     else {
         second_card_clicked = the_card;
+        the_card.off("click");
     }
     if (first_card_clicked.find(".cardFront").attr("src") == second_card_clicked.find(".cardFront").attr("src")) {
-        // This .addclass might be leftover from previous iteration and unnecessary now
         first_card_clicked.addClass("matched");
         second_card_clicked.addClass("matched");
         match_counter++;
@@ -38,14 +42,16 @@ function card_clicked() {
         }
     }
     else {
+        can_be_clicked = false;
         setTimeout(showBack, 2000);
         function showBack() {
             first_card_clicked.find('.back').show();
             second_card_clicked.find('.back').show();
-            first_card_clicked.off("click").on("click", card_clicked);  // Is this .off necessary since its done above
-            second_card_clicked.off("click").on("click", card_clicked);
+            first_card_clicked.on("click", card_clicked);
+            second_card_clicked.on("click", card_clicked);
             first_card_clicked = null;
             second_card_clicked = null;
+            can_be_clicked = true;
         }
 
         return;
