d7562290c47f2f740f2f8920f9e3779a88fc16f5
Bill Leonhardt <iamleonhardt@gmail.com>
2016-07-11 18:02:38 -0700

    v1.0 Need to resolve matches and match_counter

--- memgame.js
+++ memgame.js
@@ -3,19 +3,60 @@ var second_card_clicked = null;
 var total_possible_matches = 0;
 var match_counter = 0;
 var can_be_clicked = true;
+var matches = 0;
+var attempts = 0;
+var accuracy = 0;
+var games_played = 0;
+
 
 $(document).ready(initialize_game);
 
+// INITIALIZE GAME
 function initialize_game() {
-    $("div.card").on("click", card_clicked);
+    $("div.card").click(card_clicked);
     total_possible_matches = $(".card").length / 2;
-    // console.log('total_possible_matches : ', total_possible_matches);
+    $("button").click(reset);
 }
 
+// RESET BUTTON
+function reset() {
+    games_played++;
+    reset_stats();
+    display_stats();
+    //reset all cards to have the back face showing
+    reset_cards();
+}
+
+function reset_cards() {
+    $(".matched").find(".back").show();
+    $(".matched").off(card_clicked).click(card_clicked);
+}
+
+function display_stats() {
+    $(".games-played .value").text(games_played);
+    $(".attempts .value").text(attempts);
+    if (attempts == 0) {
+        accuracy = 0;
+    }
+    else {
+        accuracy = Math.round(matches / attempts * 100) + "%";
+    }
+    $(".accuracy .value").text(accuracy);
+}
+
+function reset_stats() {
+    accuracy = 0;
+    matches = 0;
+    attempts = 0;
+    display_stats();
+}
+
+// CARDS CLICKED
 function card_clicked() {
-    if (can_be_clicked == false){
+    if (can_be_clicked == false) {
         return;
     }
+    // FIRST CARD
     var the_card = $(this);
     the_card.find(".back").hide();
     the_card.off("click");
@@ -23,17 +64,26 @@ function card_clicked() {
         first_card_clicked = the_card;
         return;
     }
+    // SECOND CARD
     else {
         second_card_clicked = the_card;
         the_card.off("click");
+        attempts++;
+        display_stats();
     }
+
+    // CHECK FOR MATCH
     if (first_card_clicked.find(".cardFront").attr("src") == second_card_clicked.find(".cardFront").attr("src")) {
         first_card_clicked.addClass("matched");
         second_card_clicked.addClass("matched");
         match_counter++;
+        matches++;
+        display_stats();
         first_card_clicked = null;
         second_card_clicked = null;
 
+
+        // CHECK FOR WIN
         if (match_counter == total_possible_matches) {
             $("#game-area").text("Cheers, mate. You've won the game!")
         }
@@ -41,17 +91,19 @@ function card_clicked() {
             return;
         }
     }
+    // CARDS DONT MATCH
     else {
         can_be_clicked = false;
-        setTimeout(showBack, 2000);
+        setTimeout(showBack, 1200);
         function showBack() {
             first_card_clicked.find('.back').show();
             second_card_clicked.find('.back').show();
-            first_card_clicked.on("click", card_clicked);
-            second_card_clicked.on("click", card_clicked);
+            first_card_clicked.click(card_clicked);
+            second_card_clicked.click(card_clicked);
             first_card_clicked = null;
             second_card_clicked = null;
             can_be_clicked = true;
+            display_stats();
         }
 
         return;
